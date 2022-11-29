6e7915c6783825f585941543c0030cf5a818fcfb
Zoltan Fabian <zoltan.dzooli.fabian@gmail.com>
2022-11-29 12:45:40 +0100

    optimize imports

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
 import os
+import sys
 import uuid
 import requests
-import sys
 
-from flask import make_response, redirect, Flask
-from flask import request
+from flask import make_response, redirect, Flask, request
 
 app = Flask(__name__)
 
@@ -21,15 +20,19 @@ if client_secret == "":
 
 state_auth = str(uuid.uuid4()).upper()
 
+
 @app.route("/login")
 def login_with_github():
     """
     Redirection to GitHub OAuth endpoint to authorize the application and start log-in web-flow.
     """
     global client_id, client_secret, redirect_uri, state_auth
-    resp = make_response(redirect(
-        f"https://github.com/login/oauth/authorize?redirect_uri={redirect_uri}&client_id={client_id}&scope=user&state={state_auth}"))
-    resp.headers['Accept'] = 'application/json'
+    resp = make_response(
+        redirect(
+            f"https://github.com/login/oauth/authorize?redirect_uri={redirect_uri}&client_id={client_id}&scope=user&state={state_auth}"
+        )
+    )
+    resp.headers["Accept"] = "application/json"
     return resp
 
 
@@ -40,29 +43,40 @@ def authenticated_redirect_target():
     """
     # When code is received, exchange it to an access_token
     global client_id, client_secret, redirect_uri, state_auth
-    if isinstance(request.args["code"], str) \
-            and isinstance(request.args["state"], str) \
-            and request.args["state"] == state_auth:
+    if (
+        isinstance(request.args["code"], str)
+        and isinstance(request.args["state"], str)
+        and request.args["state"] == state_auth
+    ):
         try:
-            resp = requests.post("https://github.com/login/oauth/access_token", data={
-                "client_id"    : client_id,
-                "client_secret": client_secret,
-                "code"         : str(request.args["code"]),
-                "redirect_uri" : "http://localhost:8000/auth"
-            }, headers={
-                "Accept": "application/json"
-            })
+            resp = requests.post(
+                "https://github.com/login/oauth/access_token",
+                data={
+                    "client_id": client_id,
+                    "client_secret": client_secret,
+                    "code": str(request.args["code"]),
+                    "redirect_uri": "http://localhost:8000/auth",
+                },
+                headers={"Accept": "application/json"},
+                timeout=10,
+            )
         except Exception as e:
             return str(e)
         try:
             # Get the user info with the retrieved access_token
-            userinfo = requests.get("https://api.github.com/user", headers={
-                "Authorization": "token {}".format(resp.json()["access_token"]),
-                "Accept"       : "application/json"
-            })
+            userinfo = requests.get(
+                "https://api.github.com/user",
+                headers={
+                    "Authorization": "token {}".format(resp.json()["access_token"]),
+                    "Accept": "application/json",
+                },
+                timeout=10,
+            )
         except Exception as e:
             return str(e)
-        return "Welcome " + str(userinfo.json()["name"]) + "!"  # Could be a redirect to the main application page
+        return (
+            "Welcome " + str(userinfo.json()["name"]) + "!"
+        )  # Could be a redirect to the main application page
     return "Login failed!"
 
 
